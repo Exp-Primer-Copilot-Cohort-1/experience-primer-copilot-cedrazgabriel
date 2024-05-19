eec9355844d9abf3f124d845ed5d9d679f6ede70
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2024-05-19 15:09:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,5 @@
-//create web server
-//create a web server that listens to the port 3000
-//and sends a response of "Hello World" for every request it receives
-//create a web server that listens to the port 3000
-//and sends a response of "Hello World" for every request it receives
+// Create web server
+
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.end("Hello World");
